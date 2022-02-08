7f343fc482a4ca6ddee36c9ed3e6fc5424e563ee
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2022-02-08 23:54:56 +0900

    Login logout

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
+const logout = document.querySelector("#logout-form");
 
-const onLoginBtnClick = () => {
-  console.log(loginInput.value);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+const onLoginSubmit = (event) => {
+  event.preventDefault();
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings(username);
+};
+const offLogoutClick = (event) => {
+  event.preventDefault();
+  localStorage.removeItem(USERNAME_KEY);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  logout.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = ``;
 };
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+logout.addEventListener("submit", offLogoutClick);
+
+const paintGreetings = (username) => {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  logout.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+};
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
