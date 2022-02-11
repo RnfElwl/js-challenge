6ed09de148c1f7312623009b1ad955c2c9fb3770
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2022-02-12 01:54:00 +0900

    todos id add delete

--- chrome/js/todo.js
+++ chrome/js/todo.js
@@ -2,22 +2,26 @@ const toDoForm = document.querySelector("#todo-form");
 const todoList = document.querySelector("#todo-list");
 const toDoInput = document.querySelector("#todo-form input");
 
-const toDos = [];
+const TODOS_KEY = "todos";
+let toDos = [];
 
 function saveToDos() {
-  localStorage.setItem("todos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((todo) => todo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 const paintTodo = (newTodo) => {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
   const button = document.createElement("button");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
@@ -30,8 +34,20 @@ function hanleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintTodo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintTodo(newTodoObj);
   saveToDos();
 }
 toDoForm.addEventListener("submit", hanleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintTodo);
+}
