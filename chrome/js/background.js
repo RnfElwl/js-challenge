21c765e41a1ed38abb6846a1230eae85919b280c
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2022-02-14 23:13:08 +0900

    background test

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,8 +1,17 @@
-const images = ["0.png", "1.jpg", "2.jpg"];
+const images = ["0.jpg", "1.png", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+const bgColors = [
+  "#DB7093",
+  "#c7e366",
+  "#87CEFA",
+  "#FFD700",
+  "#7B68EE",
+  "#BA55D3",
+];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+const chosenImage = Math.floor(Math.random() * images.length);
 
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-document.body.style.backgroundImage = `url(../img/${chosenImage}) no-repear`;
+const bgImage = document.querySelector("#isedol");
+const bgColor = document.querySelector("body");
+bgImage.classList.add("bg-img");
+bgColor.style.backgroundColor = `${bgColors[chosenImage]}`;
+bgImage.style.backgroundImage = `url(img/${images[chosenImage]})`;
--- chrome/js/quotes.js
+++ chrome/js/quotes.js
@@ -1,27 +1,31 @@
 const quotes = [
   {
-    quote: "aaaa",
-    author: "aaaa",
+    quote:
+      "You will face many defeats in life, but never let yourself be defeated.",
+    author: "Maya Angelou",
   },
   {
-    quote: "bbb",
-    author: "bbb",
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
   },
   {
-    quote: "ccc",
-    author: "ccc",
+    quote:
+      "In the end, it's not the years in your life that count. It's the life in your years.",
+    author: "Abraham Lincoln",
   },
   {
-    quote: "ddd",
-    author: "ddd",
+    quote: "Never let the fear of striking out keep you from playing the game.",
+    author: "Babe Ruth",
   },
   {
-    quote: "eeee",
-    author: "eee",
+    quote: "Life is either a daring adventure or nothing at all.",
+    author: "Helen Keller",
   },
   {
-    quote: "fff",
-    author: "fff",
+    quote:
+      "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+    author: "Thomas A. Edison",
   },
 ];
 
